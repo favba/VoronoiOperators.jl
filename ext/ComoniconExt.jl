229ef22171e087b19ffd3c30754556688cb459a5
Felipe A. V. de Bragança Alves <favbalves@gmail.com>
2025-09-25 11:06:28 -0300

    Write `xtime` matrix to operators output netcdf file so it is readable by the atmosphere model.

--- ext/ComoniconExt.jl
+++ ext/ComoniconExt.jl
@@ -10,7 +10,36 @@ const vo = Base.get_extension(VoronoiOperators, :NCDatasetsExt)
 
 using Comonicon
 
+function save_start_time(fn::String, st::String)
+    NCDataset(fn,"a") do ds
+
+        if !haskey(ds.dim, "StrLen")
+            ds.dim["StrLen"] = 64
+        end
+
+        if !haskey(ds.dim, "Time")
+            ds.dim["Time"] = Inf #Unlimited dimension
+        end
+
+        if !haskey(ds, "xtime")
+            xtime = [' ' for i=1:64, j=1:1]
+            nl = length(st)
+            xtime[1:nl,1] .= collect(st)
+            defVar(
+                ds, "xtime", xtime,
+                ("StrLen", "Time"), attrib = [
+                    "units" => "YYYY-MM-DD_hh:mm:ss",
+                    "long_name" => "Model valid time",
+                ]
+            )
+        end
+    end
+end
+
+precompile(Tuple{typeof(save_start_time), String, String})
+
 """
+
 Create and save new Voronoi horizontal operators to a NetCDF file.
 
 # Arguments
@@ -22,8 +51,9 @@ Create and save new Voronoi horizontal operators to a NetCDF file.
 
 - `-t, --tangent_reconstruction`: Edge tangential velocity reconstruction method. Available options: "Peixoto", "PeixotoOld", "LSq1", "LSq2", and "Thuburn". Default is "Thuburn".
 - `-c, --cell_reconstruction`: Cell velocity reconstruction method. Available options: "Perot", "PerotOld", "LSq1", and "LSq2". No cell velocity reconstruction method is written to file by default.
+- `-s, --start_time`: Simulation start time. Should be the same as in "config \U0332 start \U0332 time" from namelist.init \U0332 atmosphere. Default value of "0000-01-01 \U0332 00:00:00".
 """
-Comonicon.@main function create_voronoi_operator(grid::String, output::String; tangent_reconstruction::String="Thuburn", cell_reconstruction::String="")
+Comonicon.@main function create_voronoi_operator(grid::String, output::String; tangent_reconstruction::String="Thuburn", cell_reconstruction::String="", start_time::String="0000-01-01_00:00:00")
     @info string("Reading ", grid," file")
     mesh = vm.VoronoiMesh(grid)
     @info "Creating edge tangent velocity reconstruction weights"
@@ -32,6 +62,7 @@ Comonicon.@main function create_voronoi_operator(grid::String, output::String; t
         @info "Creating cell velocity reconstruction weights"
         VoronoiOperators.save_cell_reconstruction(mesh, cell_reconstruction, output)
     end
+    save_start_time(output, start_time)
     @info string("Operators saved to ", output)
     return 0
 end

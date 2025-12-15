ce86a0af67a550b6bfd08a0668ca8c9935798147
Felipe A. V. de Bragança Alves <favbalves@gmail.com>
2025-12-15 15:18:12 -0300

    Add option to write areas to operators NetCDF file.
    Using new `areas_type` keyword parameter.

--- ext/ComoniconExt.jl
+++ ext/ComoniconExt.jl
@@ -36,6 +36,24 @@ end
 
 precompile(Tuple{typeof(save_start_time), String, String})
 
+function write_custom_areas(output::String, mesh::AbstractVoronoiMesh, areas_type)
+
+    NCDataset(output,"a") do ds
+        if areas_type == "geometric"
+            write_field_to_netcdf!(ds, mesh.cells.area, "customAreaCell", "nCells", ["units" => "m^2"])
+            write_field_to_netcdf!(ds, mesh.vertices.area, "customAreaVertex", "nVertices", ["units" => "m^2"])
+            write_field_to_netcdf!(ds, mesh.vertices.kiteAreas, "customKiteAreas", ("vertexDegree", "nVertices"), ["units" => "m^2"])
+            ds.attrib["areas_type"] = "geometric"
+        elseif areas_type == "mimetic"
+            write_field_to_netcdf!(ds, mesh.cells.areaMimetic, "customAreaCell", "nCells", ["units" => "m^2"])
+            write_field_to_netcdf!(ds, mesh.vertices.areaMimetic, "customAreaVertex", "nVertices", ["units" => "m^2"])
+            write_field_to_netcdf!(ds, mesh.vertices.kiteAreasMimetic, "customKiteAreas", ("vertexDegree", "nVertices"), ["units" => "m^2"])
+            ds.attrib["areas_type"] = "mimetic"
+        end
+    end
+
+end
+
 """
 
 # Intro
@@ -52,12 +70,14 @@ Create and save new Voronoi horizontal operators to a NetCDF file.
 - `-t, --tangent_reconstruction`: Edge tangential velocity reconstruction method. Available options: "Peixoto", "PeixotoOld", "LSq1", "LSq2", and "Thuburn". Default is "Thuburn".
 - `-c, --cell_reconstruction`: Cell velocity reconstruction method. Available options: "Perot", "PerotOld", "LSq1", and "LSq2". No cell velocity reconstruction method is written to file by default.
 - `-v, --vertex_reconstruction`: Vertex velocity reconstruction method. Available options: "Perot", "LSq1", and "LSq2". No vertex velocity reconstruction method is written to file by default.
+- `-a, --areas`: Include area data. Available options: "geometric", where the true geometrical areas are computed, or "mimetic", where areas are computed in a suitable manner to the TRiSK method. No area data is written to file by default.
 - `-s, --start_time`: Simulation start time. Should be the same as in "config \U0332 start \U0332 time" from namelist.init \U0332 atmosphere. Default value of "0000-01-01 \U0332 00:00:00".
 """
 Comonicon.@main function create_voronoi_operator(grid::String, output::String;
                                                  tangent_reconstruction::String="Thuburn",
                                                  cell_reconstruction::String="",
                                                  vertex_reconstruction::String="",
+                                                 areas::String="",
                                                  start_time::String="0000-01-01_00:00:00")
     @info string("Reading ", grid," file")
     mesh = VoronoiMesh(grid, read_computed=false)
@@ -71,13 +91,17 @@ Comonicon.@main function create_voronoi_operator(grid::String, output::String;
         @info "Creating vertex velocity reconstruction weights"
         VoronoiOperators.save_vertex_reconstruction(mesh, vertex_reconstruction, output)
     end
+    if areas != ""
+        @info "Creating area data"
+        write_custom_areas(output, mesh, areas)
+    end
     save_start_time(output, start_time)
     @info string("Operators saved to ", output)
     return 0
 end
 
-precompile(Tuple{typeof(Core.kwcall), NamedTuple{(:tangent_reconstruction, :cell_reconstruction, :vertex_reconstruction, :start_time), Tuple{String, String, String, String}}, typeof(create_voronoi_operator), String, String})
-precompile(create_voronoi_operator, (String, String, @NamedTuple{tangent_reconstruction::String, cell_reconstruction::String, vertex_reconstruction::String, start_time::String}))
+precompile(Tuple{typeof(Core.kwcall), NamedTuple{(:tangent_reconstruction, :cell_reconstruction, :vertex_reconstruction, :areas, :start_time), Tuple{String, String, String, String, String}}, typeof(create_voronoi_operator), String, String})
+precompile(create_voronoi_operator, (String, String, @NamedTuple{tangent_reconstruction::String, cell_reconstruction::String, vertex_reconstruction::String, areas::String, start_time::String}))
 
 precompile(Tuple{typeof(command_main), Vector{String}})
 precompile(Tuple{typeof(command_main)})
